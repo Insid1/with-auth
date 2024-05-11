b9b78f5e30937ba9eaab3273398a358a1e3be5ad
Oleg <66309663+Insid1@users.noreply.github.com>
2024-05-11 20:45:57 +0700

    updates project

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -1,9 +1,9 @@
 package entity
 
 type User struct {
-	Id       uint
-	Email    string
-	Name     string
-	Age      int
-	Password string
+	Id       uint   `json:"-"`
+	Email    string `json:"name" binding:"required"`
+	Name     string `json:"email" binding:"required"`
+	Age      int    `json:"age" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
--- user.go
+++
@@ -1,8 +0,0 @@
-package user
-
-type User struct {
-	Id       uint64 `json:"-"`
-	Name     string `json:"name" binding:"required"`
-	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
-}
