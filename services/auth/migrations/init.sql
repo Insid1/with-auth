ae9a73d255a262c2142ce7794ecc014884fc33be
Oleg <66309663+Insid1@users.noreply.github.com>
2024-06-19 21:35:49 +0700

    adds  DB init sql for auth service

---
+++ services/auth/migrations/init.sql
@@ -0,0 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE "token"
+(
+    id         uuid PRIMARY KEY DEFAULT uuid_generate_v1(),
+    token      varchar(255) NOT NULL,
+    user_id    varchar(255) NOT NULL,
+    created_at timestamp        DEFAULT now(),
+    updated_at timestamp        DEFAULT now()
+);
+
+
+INSERT INTO "token" (id, token, user_id)
+VALUES ('8fe7b3f4-18ae-11ef-a7a6-0242ac150002', '1', '8fe7b3f4-18ae-11ef-a7a6-0242ac150002')
+ON CONFLICT DO NOTHING;
